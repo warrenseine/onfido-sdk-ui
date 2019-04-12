56d563c745066779d5dd8c2796b2c50690c766ea
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-04-12 17:20:58 +0100

    Add ui elements of the cross device screen

--- test/js/pageobjects/CrossDevice.js
+++ test/js/pageobjects/CrossDevice.js
@@ -3,9 +3,13 @@ import Base from './BasePage.js'
 class CrossDevice extends Base{
 
   get crossDeviceTitle() { return this.$('.onfido-sdk-ui-Title-titleSpan')}
-  get crossDeviceNumberInputLabel() { return this.$('.onfido-sdk-ui-crossDevice-CrossDeviceLink-label')}
+  get crossDeviceNumberInputLabel() { return this.$('div#onfido-mount div.onfido-sdk-ui-crossDevice-CrossDeviceLink-smsSection > div.onfido-sdk-ui-crossDevice-CrossDeviceLink-label')}
   get crossDeviceNumberInput() { return this.$('.onfido-sdk-ui-crossDevice-CrossDeviceLink-label')}
-
+  get crossDeviceSendLinkBtn() { return this.$('div#onfido-mount div.onfido-sdk-ui-crossDevice-CrossDeviceLink-numberInputSection > button')}
+  get crossDeviceCopyLinkInstead() { return this.$('div#onfido-mount div.onfido-sdk-ui-crossDevice-CrossDeviceLink-copyLinkSection > div.onfido-sdk-ui-crossDevice-CrossDeviceLink-label')}
+  get crossDeviceCopyToClipboardBtn() { return this.$('.onfido-sdk-ui-crossDevice-CrossDeviceLink-copyToClipboard')}
+  get crossDeviceCopyLinkContainer() { return this.$('.onfido-sdk-ui-crossDevice-CrossDeviceLink-linkContainer')}
+  get crossDeviceDivider() { return this.$('.onfido-sdk-ui-crossDevice-CrossDeviceLink-divider')}
 
   copy = (lang="en") =>
     require(`../../../src/locales/${lang}.json`)
