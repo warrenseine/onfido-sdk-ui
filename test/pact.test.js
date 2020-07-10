0778d9f8903b515a2a0fd6a4872033c484bbb844
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-07-10 13:24:31 +0100

    Add pact

---
+++ test/pact.test.js
@@ -0,0 +1,37 @@
+import { Pact } from '@pact-foundation/pact';
+
+describe('SDK Consumer Pact Tests', () => {
+    let provider = null;
+
+    beforeAll(async () => {
+        provider = new Pact({
+            consumer: 'JS SDK',
+            provider: 'SDKToken',
+            port: 5679,
+            pactfileWriteMode: 'update'
+        });
+        await provider.setup();
+    });
+
+    describe('get applicant id', () => {
+        test('get applicant id', async () => {
+          await provider.addInteraction({
+            uponReceiving: 'a request for all queues',
+            withRequest: {
+              method: 'GET',
+              path: '/sdk_token',
+              headers: {
+                Accept: '*/*',
+                Authorization: 'BASIC NA',
+              },
+            },
+            willRespondWith: {
+              status: 200,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            },
+          });
+        });
+    });
+});
