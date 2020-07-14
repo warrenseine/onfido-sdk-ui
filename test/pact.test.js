cba7cc8b03713b880d36c7b6d5fa66e054a6fb2e
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-07-14 09:28:49 +0100

    Add publish pact script to travis.ymli

--- test/pact.test.js
+++ test/pact.test.js
@@ -1,37 +1,34 @@
 import { Pact } from '@pact-foundation/pact';
 
 describe('SDK Consumer Pact Tests', () => {
-    let provider = null;
+  let provider = null;
 
-    beforeAll(async () => {
-        provider = new Pact({
-            consumer: 'JS SDK',
-            provider: 'SDKToken',
-            port: 5679,
-            pactfileWriteMode: 'update'
-        });
-        await provider.setup();
+  beforeAll(async () => {
+    provider = new Pact({
+      consumer: 'JS SDK',
+      provider: 'SDKToken',
+      port: 5679,
+      pactfileWriteMode: 'update'
     });
+    await provider.setup();
+  });
 
-    describe('get applicant id', () => {
-        test('get applicant id', async () => {
-          await provider.addInteraction({
-            uponReceiving: 'a request for all queues',
-            withRequest: {
-              method: 'GET',
-              path: '/sdk_token',
-              headers: {
-                Accept: '*/*',
-                Authorization: 'BASIC NA',
-              },
-            },
-            willRespondWith: {
-              status: 200,
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            },
-          });
-        });
+  describe('get applicant id', () => {
+    test('get applicant id', async () => {
+      await provider.addInteraction({
+        uponReceiving: 'a request for all queues',
+        withRequest: {
+          method: 'GET',
+          path: '/sdk_token',
+          headers: {
+            Accept: '*/*',
+            Authorization: 'BASIC NA',
+          },
+        },
+        willRespondWith: {
+          status: 200,
+        },
+      });
     });
+  });
 });
--- test/scripts/publish_pact_files.js
+++ test/scripts/publish_pact_files.js
@@ -1,6 +1,5 @@
 const pact = require('@pact-foundation/pact-node');
 const path = require('path');
-
 const branch = process.env.CI_COMMIT_BRANCH;
 const commitSHA = process.env.CI_COMMIT_SHA;
 
@@ -9,15 +8,15 @@ console.info(`🌿 Branch: ${branch}`)
 console.info(`🏷️  ReleaseID: ${commitSHA}`)
 
 pact
-  .publishPacts({
-    pactBroker: 'https://pact-broker.eu-west-1.dev.onfido.xyz/',
-    pactFilesOrDirs: [path.resolve(__dirname, '../pacts')],
-    consumerVersion: commitSHA,
-    tags: [branch],
-  })
-  .then(() => {
-    console.info('Pact files published successfully!');
-  })
-  .catch(error => {
-    console.error('Pact files failed to upload: ', error.message);
-  });
+.publishPacts({
+  pactBroker: 'https://pact-broker.eu-west-1.dev.onfido.xyz/',
+  pactFilesOrDirs: [path.resolve(__dirname, '../pacts')],
+  consumerVersion: commitSHA,
+  tags: [branch],
+})
+.then(() => {
+  console.info('Pact files published successfully!');
+})
+.catch(error => {
+  console.error('Pact files failed to upload: ', error.message);
+});
