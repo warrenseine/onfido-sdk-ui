e54703e37890fe0fde46e844b9120eb385c8497f
Stefania Cardenas <stefania.cardenas@onfido.com>
2020-10-06 14:38:01 +0100

    test

--- src/demo/demo.test.js
+++ src/demo/demo.test.js
@@ -2,45 +2,30 @@ import { h } from 'preact'
 import { mount } from 'enzyme'
 import { expect } from 'chai'
 
-describe('Mount Demo App by mocking Onfido SDK', () => {
+describe('Mounting the Demo App', () => {
   let Demo = null
 
   beforeEach(() => (Demo = require('./demo.js')))
 
-  jest.mock('../index.js', () => ({
-    // eslint-disable-next-line react/display-name
-    init: () => <div />,
-  }))
+  describe('by mocking Onfido SDK', () => {
+    global.Onfido = {
+      init: () => {},
+    }
 
-  it('renders the Onfido Demo without crashing', () => {
-    // the component needs to be assigned to a lowercase variable to work!
-    const sdk = <Demo />
-    const sdkDemo = mount(<sdk />)
-    expect(sdkDemo).to.be.ok
+    it('renders the Onfido Demo without crashing', () => {
+      // the component needs to be assigned to a lowercase variable to work!
+      const sdk = <Demo />
+      const sdkDemo = mount(<sdk />)
+      expect(sdkDemo).to.be.ok
+    })
   })
-})
 
-describe('Mount Demo App without mocking Onfido SDK', () => {
-  let Demo = null
-
-  beforeEach(() => {
-    Demo = require('./demo.js')
-    // when the Onfido SDK is imported Woopra needs to be mocked
-    const mockWoopraFn = jest.fn()
-
-    jest.mock('../Tracker/safeWoopra', () =>
-      jest.fn().mockImplementation(() => ({
-        init: () => mockWoopraFn,
-        config: () => mockWoopraFn,
-        identify: () => mockWoopraFn,
-        track: () => mockWoopraFn,
-      }))
-    )
-  })
-  it('renders the Onfido Demo without crashing', () => {
-    // the component needs to be assigned to a lowercase variable to work!
-    const sdk = <Demo />
-    const sdkDemo = mount(<sdk />)
-    expect(sdkDemo).to.be.ok
+  describe('without mocking Onfido SDK', () => {
+    it('renders the Onfido Demo without crashing', () => {
+      // the component needs to be assigned to a lowercase variable to work!
+      const sdk = <Demo />
+      const sdkDemo = mount(<sdk />)
+      expect(sdkDemo).to.be.ok
+    })
   })
 })
