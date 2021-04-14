b353b6c231ce7c55ebef7b05dfdc5b18adabd190
Kimmi Gan <kimmi.gan@onfido.com>
2021-04-14 14:16:00 +0100

    Separate test spec file for testing different import(s)

---
+++ test/specs/integrationImports.js
@@ -0,0 +1,25 @@
+import { describe } from '../utils/mochaw'
+import { welcomeScenarios } from './scenarios/welcome'
+import { documentSelectorScenarios } from './scenarios/documentSelector'
+import { countrySelectorScenarios } from './scenarios/countrySelector'
+import { documentScenarios } from './scenarios/document'
+import { faceScenarios } from './scenarios/face'
+import { crossDeviceScenarios } from './scenarios/crossDevice'
+import { modalScenarios } from './scenarios/modal'
+import { navigationScenarios } from './scenarios/navigation'
+import { proofOfAddressScenarios } from './scenarios/proofOfAddress'
+import { userConsentScenarios } from './scenarios/userConsent'
+
+describe('Happy Paths on Chrome', () => {
+  // Only test for default language (US English)
+  welcomeScenarios()
+  documentSelectorScenarios()
+  countrySelectorScenarios()
+  documentScenarios()
+  faceScenarios()
+  crossDeviceScenarios()
+  modalScenarios()
+  navigationScenarios()
+  proofOfAddressScenarios()
+  userConsentScenarios()
+})
