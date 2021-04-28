70f1964aa66aa248baa736f735553ccee231b308
Kimmi Gan <kimmi.gan@onfido.com>
2021-04-28 13:01:42 +0100

    Rename UI test config file for SDK imports

--- test/specs/sdkImportMethods.js
+++ test/specs/sdkImportMethods.js
@@ -10,7 +10,7 @@ import { navigationScenarios } from './scenarios/navigation'
 import { proofOfAddressScenarios } from './scenarios/proofOfAddress'
 import { userConsentScenarios } from './scenarios/userConsent'
 
-describe('Happy Paths on Chrome', () => {
+describe('Happy Paths on Chrome for import integrations', () => {
   // Only test for default language (US English)
   welcomeScenarios()
   documentSelectorScenarios()
